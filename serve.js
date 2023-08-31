5a0bda7e4150851d4a03c6481d729b2c2070e936
Nica930 <114169534+Nica930@users.noreply.github.com>
2023-08-31 14:27:34 +0300

    Add files via upload

--- serve.js
+++ serve.js
@@ -6,57 +6,47 @@ const openai = require('./src/ai.js');
 
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded())
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 // step 1 - access the USSD
 app.get('/', (_req, res) => res.send('Welcome, Get Information on Agriculture'));
 
-// post on access - prompts 
+
 app.post('/ussd', async (req, res) => {
-    
     const { text } = req.body;
-
     let response = '';
-
     const textParts = text.split('*');
 
     if (text === '') {
-        response = 'CON Welcome to Kilimo/Karibu kwenye Kilimo\n Please select a language to continue:/Changua Lugha ili kuendelea:\n 1. English \n 2. Kiswahili\n 0. Exit/Toka'
-    } else if (textParts[0] === '1') {
+        response = 'CON Welcome to Kilimo/Karibu kwenye Kilimo...\n Please select a language to continue:\n 1. English\n 2. Kiswahili\n 0. Exit/Toka';
+    } else if (textParts[0] === '1' || textParts[0] === '2') {
+        const selectedLanguage = textParts[0]; // 1 for English, 2 for Kiswahili
+
         if (textParts.length > 1) {
             const question = textParts[1];
-            const outcome = await openai.ask(question);
-
-            if (outcome.status === 'success') {
-                response = `END ${outcome.message}`
-            } else {
-                response = 'END Sorry, an error occurred. Please try again.'
+            try {
+                const outcome = await openai.askAboutAgriculture(question);
+                response = outcome.status === 'success'
+                    ? `END ${outcome.message}`
+                    : selectedLanguage === '1' ? 'END Sorry, an error occurred. Please try again.' : 'END Samahani, kosa limetokea. Tafadhali jaribu tena.';
+            } catch (error) {
+                response = selectedLanguage === '1'
+                    ? 'END An error occurred while processing your request. Please try again.'
+                    : 'END Kuna kosa lililotukia wakati wa kusindika ombi lako. Tafadhali jaribu tena.';
             }
         } else {
-            response = 'CON Please ask any Agricultural related question. For example: How to plant maize';
-        }
-
-    } else if (textParts[0] === '2') {
-        if (textParts.length > 1) {
-            const question = textParts[1];
-            const outcome = await openai.uliza(question);
-
-            if (outcome.status === 'success') {
-                response = `END ${outcome.message}`
-            } else {
-                response = 'END Samahani, kosa limetokea. Tafadhali jaribu tena.'
-            }
-        } else {
-            response = 'CON Tafadhali uliza swali lolote kuhusu ukulima. Kwa mfano: Jinsi ya kupanda mahindi?';
+            response = selectedLanguage === '1'
+                ? 'CON Please ask any Agricultural related question. For example: How to plant maize'
+                : 'CON Tafadhali uliza swali lolote kuhusu ukulima. Kwa mfano: Jinsi ya kupanda mahindi?';
         }
     } else if (text === '0') {
         response = 'END You have exited Kilimo. Thank you for using our service./Umefunga Kilimo. Asante kwa kutumia huduma yetu';
     } else {
-        response = 'END Invalid input. Please try again/Ingizo batili. Tafadhali jaribu tena.'
-    };
+        response = 'END Invalid input. Please try again/Ingizo batili. Tafadhali jaribu tena.';
+    }
 
-    res.set('Content-Type: text/plain');
+    res.set('Content-Type', 'text/plain');
     res.send(response);
 });
 
--- src/ai.js
+++ src/ai.js
@@ -1,117 +1,58 @@
-const  axios = require('axios');
+//const { OpenAIApi } = require("openai");
+const axios = require('axios');
 require('dotenv').config();
 
-const OPENAI_API_KEY = process.env.OA_API_KEY
+/*const configuration = new OpenAIApi.Configuration({
+  apiKey: process.env.OA_API_KEY,
+});*/
+const apiKey= process.env.OA_API_KEY;
+//const openai = new OpenAIApi(configuration);
 
-const ask = (question) => {
+const askAboutAgriculture = (question) => {
   return new Promise((resolve, reject) => {
     const data = JSON.stringify({
       model: 'gpt-3.5-turbo',
       messages: [
         {
           role: 'system',
-          content: 'This assistant is trained on answering questions on Agriculture.'
+          content: 'This assistant is trained on answering questions related to Agriculture.'
         },
         {
           role: 'user',
           content: `${question}`
-        },
-        {
-            role: 'user',
-            content: 'Using 160 characters, what is the answer?'
-          },
-        
-        
+        }
       ],
-      temperature: 1,
-      top_p: 1,
-      n: 1,
-      stream: false,
-      max_tokens: 250,
-      presence_penalty: 0,
-      frequency_penalty: 0
-    })
+      temperature: 0.7,
+      max_tokens: 150
+    });
 
     const config = {
       method: 'post',
       maxBodyLength: Infinity,
-      url: 'https://api.openai.com/v1/chat/completions', 
+      url: 'https://api.openai.com/v1/chat/completions',
       headers: {
         'Content-Type': 'application/json',
-        'Authorization': `Bearer ${OPENAI_API_KEY}`,
+        'Authorization': `Bearer ${apiKey}`,
       },
       data
-    }
+    };
 
     axios.request(config)
       .then((response) => {
-        console.log(JSON.stringify(response.data))
-        console.log(response.data.choices[0].message.content)
-        resolve({ status: 'success', message: response.data.choices[0].message.content })
+        const answer = response.data.choices[0].message.content.trim();
+        resolve(answer);
       })
       .catch((error) => {
-        console.log("you got an error")
-        console.error(error)
-        resolve({ status: 'error', message: error })
-      })
-  })
-}
-
-const uliza = (question) => {
-    return new Promise((resolve, reject) => {
-      const data = JSON.stringify({
-        model: 'gpt-3.5-turbo',
-        messages: [
-          {
-            role: 'system',
-            content: 'This assistant is trained on answering questions on Diabetes.'
-          },
-          {
-            role: 'user',
-            content: `${question}`
-          },
-          {
-              role: 'user',
-              content: 'Using 160 characters, what is the answer in Kiswahili?'
-            },
-          
-          
-        ],
-        temperature: 1,
-        top_p: 1,
-        n: 1,
-        stream: false,
-        max_tokens: 250,
-        presence_penalty: 0,
-        frequency_penalty: 0
-      })
-  
-      const config = {
-        method: 'post',
-        maxBodyLength: Infinity,
-        url: 'https://api.openai.com/v1/chat/completions', 
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${OPENAI_API_KEY}`,
-        },
-        data
-      }
-  
-      axios.request(config)
-        .then((response) => {
-          console.log(JSON.stringify(response.data))
-          console.log(response.data.choices[0].message.content)
-          resolve({ status: 'success', message: response.data.choices[0].message.content })
-        })
-        .catch((error) => {
-          console.log("you got an error")
-          console.error(error)
-          resolve({ status: 'error', message: error })
-        })
-    })
-  }
+        console.error('Axios Error:', error.message);
+        if (error.response) {
+          console.error('Status Code:', error.response.status);
+          console.error('Response Data:', error.response.data);
+        }
+        reject(error);
+      });
+  });
+};
 
 module.exports = {
-     ask: ask,
-     uliza:uliza,
-  };
+  askAboutAgriculture: askAboutAgriculture
+};
